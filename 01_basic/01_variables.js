6e6b108fb31a962152339de71173452ee581c598
rakeshsolapure <143260898+rakeshsolapure@users.noreply.github.com>
2023-08-27 09:39:53 +0000

    variable and datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 111395
+let accountEmail = "rakesh@gmail.com "
+var accPassword = "1234"
+accountCity = "jaipur"
+let accoutntState;
+
+
+accountEmail = "hdfc@sk.com"
+accPassword ="324143245"
+accountCity = "benguluru"
+
+console.table([accountId,accountEmail, accPassword, accountCity,accoutntState]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) //we are using nodejs
+
+console.log(3 
+    +
+     3);//code shd be in readablity 
+
+     console.log("rakesh");
+
+     let name ="rakesh"
+     let  age = 18
+     let isLoogedIn = false
+     let state;
+
+     //number => 2 to power 23
+     //bigint 
+     //string => ""
+     //Boolean=>true/false
+     //null => standalone value
+     //undefined=>
+
+     console.log(typeof undefined);//underined
+     console.log(typeof null);//object
