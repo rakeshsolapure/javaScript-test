0aa41b5252613108c4fe52f56aeef487c0de2d51
rakeshsolapure <143260898+rakeshsolapure@users.noreply.github.com>
2023-08-27 09:56:29 +0000

    conversion of datatypes

---
+++ 01_basic/03_consversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33and" => NaN
+//true => 1; flase=>0
+
+let isLoggedIn = "rakesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+//""=>false
+//"rakesh"=> true
+
+let someNumber = 77
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
